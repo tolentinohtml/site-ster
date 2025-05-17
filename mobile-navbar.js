8d43a84bfd1bd9a052f20fad1f2e4dd036d00ecc
Luiz Guilherme Tolentino <luizguilhermetolentino0099@gmail.com>
2025-05-17 00:18:53 -0300

    commit 2

---
+++ mobile-navbar.js
@@ -0,0 +1,57 @@
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
+const botoes = document.querySelectorAll("#lista-linguas button");
+const detalhes = document.querySelectorAll(".detalhe-lingua");
+
+botoes.forEach(botao => {
+  botao.addEventListener("click", () => {
+    // Esconde todas as seções
+    detalhes.forEach(div => div.style.display = "none");
+
+    // Pega o id da língua clicada e mostra ela
+    const id = botao.getAttribute("data-lingua");
+    document.getElementById(id).style.display = "block";
+  });
+});
